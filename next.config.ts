b3008cc778edebcc4384bbeadd535a4a986dee53
lower <lowerfield469@gmail.com>
2025-11-17 10:12:46 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve <dy

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    return config;
+  },
 };
 
 export default nextConfig;
