69ad4d0e61d20aabe82c853578e8f80fff623371
lower <lowerfield469@gmail.com>
2025-11-25 10:26:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8742723834-1421e",
+  "appId": "1:665625375719:web:d680c46b80cd64e3578342",
+  "apiKey": "AIzaSyAX1j0On74HzyDrwRIrnRIcZPHDICBN-M0",
+  "authDomain": "studio-8742723834-1421e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "665625375719"
+};
